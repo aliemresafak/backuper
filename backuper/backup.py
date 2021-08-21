d1456a8232654594250298990216bf598003acbe
Ali Emre Şafak <aliemresafak@gmail.com>
2021-08-21 15:21:09 +0300

    Add: create folder structure in database.yaml

--- backuper/backup.py
+++ backuper/backup.py
@@ -1,16 +1,23 @@
 from os import path, makedirs
 from pathlib import Path
 from backuper.config import BACKUP_PATH
-
+from .parser import YamlParser
 class Backup:
-    def __init__(self, backup_path: Path, table_names: list[str]):
-        self.backup_path = backup_path
-        self.table_names = table_names 
-
+    def __init__(self, data: YamlParser.parse ):
+        self.backup_path = BACKUP_PATH
+        self.data = data
         self._generate_folder()
     
     def _generate_folder(self):
         # Check backup folder
         if not path.exists(BACKUP_PATH):
             makedirs(BACKUP_PATH)
-        
+        for db_name in self.data:
+            if not path.exists(BACKUP_PATH / db_name):
+                makedirs(BACKUP_PATH / db_name)
+            for table_name in self.data[db_name]:
+                if not path.exists(BACKUP_PATH / db_name / table_name):
+                    makedirs(BACKUP_PATH / db_name / table_name)
+    
+    def _backup(self):
+        ...
